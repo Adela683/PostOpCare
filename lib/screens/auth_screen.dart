f1ee793e5006414cf80f44c716f930c2319f0084
Adela683 <talevici_adela@yahoo.com>
2025-04-28 17:04:09 +0300

    Created Auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import '../widgets/custom_button.dart';
+import '../widgets/custom_button.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -23,10 +23,39 @@ class _AuthScreenState extends State<AuthScreen> {
             children: [
               Container(
                 padding:const EdgeInsets.all(30),
-                child:FlutterLogo(size:120))
+                child:Image.asset(
+                  'assets/images/PostOpCare.png',
+                  height: 300,
+                )
+              ),
+              const SizedBox(height: 50),
+              CustomButton(
+                  text: 'Sign In',
+                  onPressed: () {
+                    // Handle sign in
+                  },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(child:Divider(color: const Color.fromARGB(255, 48, 42, 42), thickness: 1.5)),
+                  Padding(padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: const Text('or', style: TextStyle(color: Color.fromARGB(255, 48, 42, 42), fontSize: 16))
+                  ),
+                  Expanded(child:Divider(color: const Color.fromARGB(255, 48, 42, 42), thickness: 1.5)),
+                ],
+              ),
+              const SizedBox(height: 20),
+              CustomButton(
+                  text: 'Log in',
+                  onPressed: () {
+                    // Handle sign up
+                  },
+              ),
             ]
-          ))
           )
+         )
+      )
     );
-}
+  }
 }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget{
+  final String text;
+  final VoidCallback onPressed;
+  
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blueAccent,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+}
